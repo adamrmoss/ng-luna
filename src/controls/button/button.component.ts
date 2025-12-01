c40a7a43c36f51131a9da5031526c5167e4b988d
Adam R Moss <adamrmoss@gmail.com>
2025-12-01 10:31:47 -0500

    Cleanup on button component

--- src/controls/button/button.component.ts
+++ src/controls/button/button.component.ts
@@ -2,6 +2,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export type ButtonType = 'button' | 'submit' | 'reset';
 
+export type FormEnctype = 'application/x-www-form-urlencoded' | 'multipart/form-data' | 'text/plain';
+
+export type FormMethod = 'get' | 'post';
+
+export type FormTarget = '_self' | '_blank' | '_parent' | '_top';
+
+export type PopoverTargetAction = 'show' | 'hide' | 'toggle';
+
 @Component({
     selector: 'luna-button',
     standalone: true,
@@ -11,22 +19,52 @@ export type ButtonType = 'button' | 'submit' | 'reset';
 export class ButtonComponent
 {
     @Input()
-    public type: ButtonType = 'button';
+    public autofocus = false;
+
+    @Input()
+    public command?: string;
+
+    @Input()
+    public commandfor?: string;
 
     @Input()
     public disabled = false;
 
     @Input()
+    public form?: string;
+
+    @Input()
+    public formaction?: string;
+
+    @Input()
+    public formenctype?: FormEnctype;
+
+    @Input()
+    public formmethod?: FormMethod;
+
+    @Input()
+    public formnovalidate = false;
+
+    @Input()
+    public formtarget?: FormTarget;
+
+    @Input()
     public name?: string;
 
     @Input()
-    public value?: string;
+    public popovertarget?: string;
 
     @Input()
-    public autofocus = false;
+    public popovertargetaction?: PopoverTargetAction;
 
     @Input()
     public tabindex?: number;
+
+    @Input()
+    public type: ButtonType = 'button';
+
+    @Input()
+    public value?: string;
 
     @Output()
     public click = new EventEmitter<MouseEvent>();
