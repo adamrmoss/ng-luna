d4ec50c4e6a3981154c944be515361a10d817c6e
Adam R Moss <adamrmoss@gmail.com>
2025-12-14 15:51:50 -0500

    Added dev app for local testing and fix font handling

---
+++ dev/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { WindowComponent } from 'ng-luna';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [ CommonModule, WindowComponent ],
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent
+{
+    public title = 'EduBASIC';
+}
+
